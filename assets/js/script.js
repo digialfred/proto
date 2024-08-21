bd60a0289f1ef98fe4e5552a28ec1a83d3f1d4aa
digialfred <digialfred@gmail.com>
2024-08-20 22:30:19 -0400

    1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,18 @@
 var tareas = [
-    { tarea: "Pintar la fachada de la casa" },
-    { tarea: "Comprar comida para el perro" },
-    { tarea: "Pagar la tarjeta de crédito" }
-]
+  { tarea: "Pintar la fachada de la casa" },
+  { tarea: "Comprar comida para el perro" },
+  { tarea: "Pagar la tarjeta de crédito" },
+];
 
-const form1 = document.getElementById('formulario');
+const form1 = document.getElementById("formulario");
 var semuestra = false;
 
-document.getElementById('agregartarea').addEventListener('click', function () {
-    if (semuestra == false) {
-        form1.style.display = ('block');
-        semuestra = true;
-    }
-    else {
-        form1.style.display = ('none');
-        semuestra= false;
-    }
-
+document.getElementById("agregartarea").addEventListener("click", function () {
+  if (semuestra == false) {
+    form1.style.display = "block";
+    semuestra = true;
+  } else {
+    form1.style.display = "none";
+    semuestra = false;
+  }
 });
-
-
-
-
-
-
